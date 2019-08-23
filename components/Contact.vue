aca185d4a45fe5daefcb99c384d6980647fbbadc
Rene Hernandez <rhernandez@onenorth.com>
2019-08-23 17:13:17 -0500

    fixing broken application

--- components/Contact.vue
+++ components/Contact.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="contact" class="contact section">
     <div class="container">
+
+
+
       <h2 class="work__title type__h2">{{heading}}</h2>
       
     </div>    
@@ -11,7 +14,7 @@
 
 export default {
   components: {
-    ImageGrid
+    
   },
   props: {
     heading: String,
@@ -32,37 +35,6 @@ export default {
 
   @include screen-above('tablet') {
    
-  }
-}
-</style>
-
-<style lang="scss">
-.services {
-  &__listing {
-    ul {
-      columns: 2 auto;
-      column-gap: $spacing__sm;
-    }
-
-    li {
-      break-inside: avoid;
-      overflow: hidden;
-      position: relative;
-      padding-left: $spacing__bt;
-      padding-bottom: $spacing__sm; 
-
-      &:before {
-        content: '';
-        position: absolute;
-        display: block;
-        left: 0;
-        width: 1.2em;
-        height: 1.2em;
-        background-size: contain;
-        background: url("~assets/sprite/svg/checkmark.svg") no-repeat top left;
-        background-size: 16px 16px;
-      }
-    }
   }
 }
 </style>
